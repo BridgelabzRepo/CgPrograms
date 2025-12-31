f3adcbc722e97dc9418e23d4bee09490d8944d63
Prabhat Chauhan <prabhat.chauhan@bridgelabz.com>
2025-12-31 12:15:31 +0530

    Adding level 1 questions

---
+++ Level1/Question1.java
@@ -0,0 +1,11 @@
+package Level1;
+
+public class Question1 {
+	int birthDate = 2000;
+	int currentYear = 2024;
+	
+	public static void main(String[] args) {
+		System.out.println("Harry's age in 2024 is = "+(2024-2000));
+	}
+
+}
